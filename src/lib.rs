4a5614742782efbc1ef8786d9a9e4f5473bd771a
Petr Pudlak <petr.mvd+git@gmail.com>
2020-04-11 11:39:02 +0000

    Expand tabs to spaces

--- src/lib.rs
+++ src/lib.rs
@@ -11,69 +11,69 @@ mod tests {
     use test_util::tests::Channel;
 
     pub struct Server {
-	handle: Option<JoinHandle<()>>,
+        handle: Option<JoinHandle<()>>,
     }
 
     impl Drop for Server {
-	fn drop(&mut self) {
-	    if !thread::panicking() {
-		self.handle.take().unwrap().join().unwrap();
-	    }
-	}
+        fn drop(&mut self) {
+            if !thread::panicking() {
+                self.handle.take().unwrap().join().unwrap();
+            }
+        }
     }
 
     impl Server {
-	fn builder(channel: Channel) -> Builder {
-	    let mut ctx = SslContext::builder(SslMethod::dtls()).unwrap();
-	    ctx.set_certificate_chain_file("test/cert.pem").unwrap();
-	    ctx.set_private_key_file("test/key.pem", SslFiletype::PEM)
-		.unwrap();
+        fn builder(channel: Channel) -> Builder {
+            let mut ctx = SslContext::builder(SslMethod::dtls()).unwrap();
+            ctx.set_certificate_chain_file("test/cert.pem").unwrap();
+            ctx.set_private_key_file("test/key.pem", SslFiletype::PEM)
+                .unwrap();
 
-	    Builder {
-		ctx,
+            Builder {
+                ctx,
                 channel,
-	    }
-	}
+            }
+        }
     }
 
     pub struct Builder {
-	ctx: SslContextBuilder,
+        ctx: SslContextBuilder,
         channel: Channel,
     }
 
     impl Builder {
 
-	pub fn build(self) -> Server {
-	    let ctx = self.ctx.build();
+        pub fn build(self) -> Server {
+            let ctx = self.ctx.build();
             let channel = self.channel;
 
             let thread = thread::Builder::new().name(String::from("server"));
-	    let handle = thread.spawn(move || {
-		    let ssl = Ssl::new(&ctx).unwrap();
-		    println!("Server is waiting for a SSL connection");
-		    let r = ssl.accept(channel);
-		    println!("Accept call result: {:?}", r.as_ref().err());
-		    let mut socket = r.unwrap();
-		    socket.write_all(b"Test message").unwrap();
-		    println!("Server finished.");
-	    }).unwrap();
+            let handle = thread.spawn(move || {
+                    let ssl = Ssl::new(&ctx).unwrap();
+                    println!("Server is waiting for a SSL connection");
+                    let r = ssl.accept(channel);
+                    println!("Accept call result: {:?}", r.as_ref().err());
+                    let mut socket = r.unwrap();
+                    socket.write_all(b"Test message").unwrap();
+                    println!("Server finished.");
+            }).unwrap();
 
-	    Server {
-		handle: Some(handle),
-	    }
-	}
+            Server {
+                handle: Some(handle),
+            }
+        }
     }
 
     #[test]
     fn verify_fingerprint() {
-	use openssl::hash::MessageDigest;
-	use openssl::ssl::{SslMethod, SslConnector, SslVerifyMode};
+        use openssl::hash::MessageDigest;
+        use openssl::ssl::{SslMethod, SslConnector, SslVerifyMode};
 
         let (server_channel, client_channel) = Channel::create_pair(0);
 
         let _server = Server::builder(server_channel).build();
 
-	let mut connector = SslConnector::builder(SslMethod::dtls()).unwrap();
+        let mut connector = SslConnector::builder(SslMethod::dtls()).unwrap();
         connector.set_verify_callback(SslVerifyMode::PEER,
                                       move |_preverified, context| {
             let fingerprint = context.current_cert().unwrap().digest(MessageDigest::sha256()).unwrap();
@@ -82,11 +82,11 @@ mod tests {
         });
         let connector = connector.build();
 
-	let stream = connector.connect("UNSPECIFIED_DOMAIN", client_channel);
+        let stream = connector.connect("UNSPECIFIED_DOMAIN", client_channel);
         println!("Connection result: {:?}", stream.as_ref().err());
         let mut stream = stream.unwrap();
-	let mut res = vec![];
-	stream.read_to_end(&mut res).unwrap();
-	println!("Client received: {}", String::from_utf8_lossy(&res));
+        let mut res = vec![];
+        stream.read_to_end(&mut res).unwrap();
+        println!("Client received: {}", String::from_utf8_lossy(&res));
     }
 }
