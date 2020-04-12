7d7f54b22b9842247a0c49fc79bd214a4d54e39b
Petr Pudlak <petr.mvd+git@gmail.com>
2020-04-12 08:46:22 +0000

    Create trait that describes SSL parameters and verification function

--- src/lib.rs
+++ src/lib.rs
@@ -19,25 +19,52 @@ impl FingerprintChain {
     }
 }
 
-pub fn connect<S, F>(stream: S, verify: F) -> Result<openssl::ssl::SslStream<S>, openssl::ssl::HandshakeError<S>>
-where S: std::io::Read + std::io::Write,
-      F: Fn(&FingerprintChain) -> bool + 'static + Sync + Send,
+pub trait Configuration {
+    fn configure(self, &mut openssl::ssl::SslContextBuilder) -> Result<(), openssl::error::ErrorStack>;
+}
+
+// TODO: Investigate the option of returning a Result from 'F' and passing errors to ErrorStack.
+fn set_verify_fingerprint_callback<F>(context: &mut openssl::ssl::SslContextBuilder, callback: F)
+where F: Fn(&FingerprintChain) -> bool + 'static + Sync + Send,
 {
-    use openssl::ssl::{SslMethod, SslConnector, SslVerifyMode};
-    let mut connector = SslConnector::builder(SslMethod::dtls())?;
-    connector.set_verify_callback(SslVerifyMode::PEER,
-                                  move |_preverified, context| {
-        return match context.chain() {
-            None => return false,
-            Some(context_chain) => match ::FingerprintChain::from_cert_chain(context_chain) {
-                Err(err) => {
-                    debug!("Unable to compute fingerprints of a certificate chain: {:?}", err);
-                    return false;
+    context.set_verify_callback(
+        openssl::ssl::SslVerifyMode::PEER, move |_preverified, ctx| {
+            match ctx.chain() {
+                None => false,
+                Some(context_chain) => match FingerprintChain::from_cert_chain(context_chain) {
+                    Err(err) => {
+                        debug!("Unable to compute fingerprints of a certificate chain: {:?}", err);
+                        false
+                    },
+                    Ok(chain) => callback(&chain),
                 },
-                Ok(chain) => verify(&chain),
-            },
-        };
-    });
+            }
+        })
+}
+
+pub struct SimpleConfiguration {
+    allowed_keys: std::collections::HashSet<Vec<u8>>,
+}
+
+impl Configuration for SimpleConfiguration {
+    fn configure(self, context: &mut openssl::ssl::SslContextBuilder) -> Result<(), openssl::error::ErrorStack> {
+        set_verify_fingerprint_callback(context, move |chain| -> bool {
+            match chain.chain.first() {
+                Some(digest) if self.allowed_keys.contains(digest.as_ref()) => true,
+                _ => false,
+            }
+        });
+        Ok(())
+    }
+}
+
+pub fn connect<S, C>(stream: S, config: C) -> Result<openssl::ssl::SslStream<S>, openssl::ssl::HandshakeError<S>>
+where S: std::io::Read + std::io::Write,
+      C: Configuration
+ {
+    use openssl::ssl::{SslMethod, SslConnector};
+    let mut connector = SslConnector::builder(SslMethod::dtls())?;
+    config.configure(&mut connector)?;
     return connector.build().connect("UNSPECIFIED_DOMAIN", stream);
 }
 
@@ -46,7 +73,7 @@ mod tests {
     use std::io::{Error, ErrorKind, Read, Write};
     use std::thread::{self, JoinHandle};
 
-    use openssl::ssl::{ShutdownResult, Ssl, SslContext, SslContextBuilder, SslFiletype, SslMethod, SslStream};
+    use openssl::ssl::{ShutdownResult, SslAcceptor, SslAcceptorBuilder, SslFiletype, SslMethod, SslStream};
     use test_util::tests::Channel;
 
     // Checks the SSL error, and if it is ZERO_RETURN (which means the connection has been closed
@@ -88,35 +115,34 @@ mod tests {
 
     impl Server {
         fn builder(channel: Channel) -> Builder {
-            let mut ctx = SslContext::builder(SslMethod::dtls()).unwrap();
-            ctx.set_certificate_chain_file("test/cert.pem").unwrap();
-            ctx.set_private_key_file("test/key.pem", SslFiletype::PEM)
+            // See https://wiki.mozilla.org/Security/Server_Side_TLS
+            let mut builder = SslAcceptor::mozilla_intermediate_v5(SslMethod::dtls()).unwrap();
+            builder.set_certificate_chain_file("test/cert.pem").unwrap();
+            builder.set_private_key_file("test/key.pem", SslFiletype::PEM)
                 .unwrap();
 
             Builder {
-                ctx,
+                builder,
                 channel,
             }
         }
     }
 
     pub struct Builder {
-        ctx: SslContextBuilder,
+        builder: SslAcceptorBuilder,
         channel: Channel,
     }
 
     impl Builder {
 
         pub fn build(self) -> Server {
-            let ctx = self.ctx.build();
+            let acceptor = self.builder.build();
             let channel = self.channel;
 
             let thread = thread::Builder::new().name(String::from("server"));
             let handle = thread.spawn(move || {
-                    // TODO: Use SslAcceptor here.
-                    let ssl = Ssl::new(&ctx).unwrap();
                     println!("Server is waiting for a SSL connection");
-                    let r = ssl.accept(channel);
+                    let r = acceptor.accept(channel);
                     println!("Accept call result: {:?}", r.as_ref().err());
                     let mut socket = r.unwrap();
                     socket.write_all(b"Test message").unwrap();
@@ -137,15 +163,17 @@ mod tests {
 
         let _server = Server::builder(server_channel).build();
 
-        let mut stream = ::connect(client_channel, move |chain| {
-            println!("Fingerprint chain: {:?}", chain);
-            assert_eq!(chain.chain.len(), 1);
-            return chain.chain[0].as_ref() == [71, 18, 185, 57, 251, 203, 66, 166, 181, 16, 27, 66, 19, 154, 37, 177, 79, 129, 180, 24, 250, 202, 189, 55, 135, 70, 241, 47, 133, 204, 101, 68];
-        }).unwrap();
+        println!("Client is connecting to the server");
+        let mut config = ::SimpleConfiguration {
+            allowed_keys: std::collections::HashSet::new(),
+        };
+        config.allowed_keys.insert([71, 18, 185, 57, 251, 203, 66, 166, 181, 16, 27, 66, 19, 154, 37, 177, 79, 129, 180, 24, 250, 202, 189, 55, 135, 70, 241, 47, 133, 204, 101, 68].to_vec());
+        let mut stream = ::connect(client_channel, config).unwrap();
+        println!("Client is receiving data");
         let mut res = vec![];
         stream.read_to_end(&mut res).unwrap();
         println!("Client received: {}", String::from_utf8_lossy(&res));
-        println!("Client shutting down.");
+        println!("Client is shutting down.");
         gracefully_shutdown(&mut stream).unwrap();
         println!("Client finished");
     }
